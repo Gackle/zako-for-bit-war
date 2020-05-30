cb2c338777fafb14671e682c4ed9cf6a68c1c393
Gackle <gackle83@outlook.com>
2020-05-31 05:13:34 +0800

    UPDATE use poetry management

--- webapp/config.py
+++ webapp/config.py
@@ -9,7 +9,7 @@ class DevConfig(Config):
     DEBUG = True
     JSON_AS_ASCII = False   # 解决 rest framework 中文乱码
     # SQLALCHEMY_DATABASE_URI = "sqlite:///../bitwar.db"
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:123123@192.168.0.202:6608/bitwar"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@192.168.1.150:3306/bitwar"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     CSRF_ENABLED = True
     SECRET_KEY = '919e08e1a692e0130b0c0e600e5f09b7'
